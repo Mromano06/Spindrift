86b46473c8cd8a3d59a1c013c396de0b9b57f79a
Mromano06 <matbat2205@gmail.com>
2025-03-27 10:46:55 -0400

    Removed extra stuff used in testing in the main class

--- Spindrift/main.c
+++ Spindrift/main.c
@@ -1,12 +1,9 @@
 #include "Menus.h"
-#include "Gameboard.h"
 #include <stdio.h>
 
 // Matthew Romano - March 24th, 2025 - Project Spindrift
 // Main project implementation
 
 int main(void) {
-	Gameboard test = createGameboard();
-	displayGameboard(test);
-	// mainMenuStart();
+	mainMenuStart();
 }
